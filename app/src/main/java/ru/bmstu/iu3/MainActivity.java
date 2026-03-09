757a69da754b16a0bff300ce76f78f231da4c40a
GermaValid <georgekolovatov@yandex.ru>
2026-03-09 03:37:11 +0300

    DevCommit!

--- app/src/main/java/ru/bmstu/iu3/MainActivity.java
+++ app/src/main/java/ru/bmstu/iu3/MainActivity.java
@@ -23,7 +23,7 @@ import org.apache.commons.codec.binary.Hex;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
     ActivityResultLauncher<Intent> activityResultLauncher;
     // Used to load the 'iu3' library on application startup.
@@ -69,14 +69,47 @@ public class MainActivity extends AppCompatActivity {
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-                            String pin = result.getData().getStringExtra("pin");
-                            Toast.makeText(MainActivity.this, pin + " Woof!", Toast.LENGTH_SHORT).show();
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
                         }
                     }
-                }
-        );
+                });
 
+
+
+    }
+
+    //Part-3
+    private String pin;
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
     }
 
     /**
@@ -89,6 +122,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
+
+    public native boolean transaction(byte[] trd);
 
     public static byte[] stringToHex(String s)
     {
@@ -104,11 +139,11 @@ public class MainActivity extends AppCompatActivity {
         return hex;
     }
 
-    public void onButtonClick(View v)
-    {
-        Intent it = new Intent(this, PinpadActivity.class);
-        //startActivity(it);
-        activityResultLauncher.launch(it);
+    public void onButtonClick(View v) {
+                byte[] trd = stringToHex("9F0206000000000100");
+                boolean ok = transaction(trd);
     }
+
+
 
 }
